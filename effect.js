2d2c2c9b1838cd3921187b99b0be388a8318217a
derbychan <65055328+derbychan@users.noreply.github.com>
2020-05-09 13:27:27 +0100

    Update effect.js

--- effect.js
+++ effect.js
@@ -192,7 +192,7 @@ $('document').ready(function () {
         });
         var i;
         function msgLoop(i) {
-            $("p:nth-child(" + i + ")").fadeOut('slow').delay(4200).promise().done(function () {
+            $("p:nth-child(" + i + ")").fadeOut('slow').delay(1200).promise().done(function () {
                 i = i + 1;
                 $("p:nth-child(" + i + ")").fadeIn('slow').delay(4500);
                 if (i == 50) {
